38876def66518c064fa7822ace0789891a0d447f
abdou_hl <abderrahmane.36.hellal@gmail.com>
2022-12-19 11:40:14 +0100

    add likes function

--- like.py
+++ like.py
@@ -3,8 +3,13 @@ from os.path import join, dirname
 from dotenv import load_dotenv
 import requests
 import os
+from deta import Deta
 
 load_dotenv(join(dirname(__file__), '.env'))
+
+deta = Deta(os.environ.get("DETA_KEY"))
+done_tyler_likes = deta.Base("done_tyler_likes")
+
 
 twitter_auth_keys = {
 	"consumer_key"        : os.environ.get("TYLER_BOT_CONSUMER_KEY"),
@@ -23,13 +28,23 @@ auth.set_access_token(
         )
 api = tweepy.API(auth)
 
-
-req = requests.get('https://api.quotable.io/random?tags=business|leadership|inspirational|success|motivational')
-data =req.json()
-tweet = f'''"{data['content']}"--{data['author']}\n\n #{data['authorSlug'].replace('-','')} {"#"+" #".join(list(map(lambda t:t.replace('-',''),data['tags'])))}'''[:280]
-api.update_status(status=tweet)
-
-
+influencers = ["GreatestQuotes","BrainyQuote","MakeItAQuote"]
+done=0
+for influencer in influencers:
+	for tweet in tweepy.Cursor(api.search_tweets, q=f'to:{influencer}',result_type='recent').items(200):
+		p = done_tyler_likes.fetch({"value": tweet.id_str})
+		if p.count != 0:
+			continue
+		try:
+			api.create_favorite(tweet.id_str)
+		except:
+			continue
+		print('tweet',tweet.id_str)
+		done_tyler_likes.put(tweet.id_str)
+		done=1
+		break
+	if done ==1:
+	    break
 
 
 
